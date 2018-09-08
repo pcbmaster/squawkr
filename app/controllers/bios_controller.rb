39b9eeafbae52e997cf9f472adfc2173b894615e
Rachel Inkala <rachelinkala@gmail.com>
2018-09-08 11:43:59 -0600

    update bio controller

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -1,28 +1,44 @@
-class BiosController < ApplicationController[
-  before_action :set_bios
+class BiosController < ApplicationController
+  before_action :set_user
+  before_action :set_bio, only: [:show, :edit, :update]
 
   def index
-    @bios = Bios.all 
+    @bios = @user.bio 
   end 
 
-  def create
-    @bios = current_user.bios.new(bio_params)
+  def show
+  end
 
-    if @bios.save
+  def new
+    @bio = current_user.bios.new
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def create
+    @bio = current_user.bios.new(bio_params)
+
+    if @bio.save
       redirect_to bios_path
     else   
       render :new
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
 
   private 
-    def bios_params
-      params.require(:bios).permit(:age, :description, :location)
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_bio
+      @bio = Bio.find(params[:id])
+    end
+
+    def bio_params
+      params.require(:bio).permit(:age, :description, :location)
     end 
 end
