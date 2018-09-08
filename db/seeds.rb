8d37826cf081ceb3b9f4f7e7e439b60fbb5bc53f
pcbmaster <fitnessnonsense@gmail.com>
2018-09-08 14:46:25 -0600

    seeds.rb now working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ posts_json = File.read('db/posts.json')
 names = JSON.parse(names_json)
 posts = JSON.parse(posts_json)
 
-posts_pos = 0
+@posts_pos = 0
 names.each do |name|
-  user = User.create(email: "#{name["first"]}#{name["last"]}@gmail.com", first_name: name["first"], last_name: name["last"], password: "test")
+  user = User.create(email: "#{name["first"]}#{name["last"]}@gmail.com", first_name: name["first"], last_name: name["last"], password: "testing")
   10.times do |i|
-    Comment.create(body: posts[post_pos], user_id: user.id)
-    posts_pos = posts_pos + 1
+    Comment.create(body: posts[@posts_pos], user_id: user.id)
+    @posts_pos = @posts_pos + 1
   end
 end
