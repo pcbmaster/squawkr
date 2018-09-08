e26566d295f58749a100eefacb9c64489f3eae70
Aramuad <samcballard@gmail.com>
2018-09-08 10:11:33 -0600

    add models for radical functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_one :bio
+  has_many :posts
+  has_many :commments
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
