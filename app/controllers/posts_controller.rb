26b5a5d542864786686cd0a1e48e1a9d5a7d8c15
Rachel Inkala <rachelinkala@gmail.com>
2018-09-08 10:12:39 -0600

    post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,36 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :destroy]
+
   def show
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
+
+  private
+
+    def before_action
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+
 end
